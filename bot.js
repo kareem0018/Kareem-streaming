b5ebb52dddcbcbcf4e1a235298b796c8e0f767f2
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-10-10 14:53:20 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
+client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
 client.user.setGame(Raaa7 al zeeeeeeen <\3,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(,'https://www.twitch.tv/v5bz%27);
@@ -9,4 +9,4 @@ client.user.setGame(Raaa7 al zeeeeeeen <\3,'https://www.twitch.tv/tarikrs');    
 	
 	
 	
-	client.login(process.env.BOT_TOKEN);
+	
