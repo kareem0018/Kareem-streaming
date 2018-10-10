f9f1aca2424263a8bbd6200a51b02fbd1fb90df7
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-10-10 14:53:34 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,3 @@ client.login(process.env.BOT_TOKEN);
 client.on('ready', () => {
 client.user.setGame(Raaa7 al zeeeeeeen <\3,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(,'https://www.twitch.tv/v5bz%27);
 });
-	
-	
-	
-	
