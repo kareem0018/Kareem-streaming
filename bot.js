c5976c7f16579c32f110babcc849371a5d0f7fbd
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-10-27 19:59:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
-const A7MD = new Discord.Client();
+const client = new Discord.Client();
 
 console.log("BOT ONLINE");
 
 
-A7MD.on("guildMemberAdd", member => {
+client.on("guildMemberAdd", member => {
   member.createDM().then(function (channel) {
   return channel.send(`***Konex shop***
 متجر لبيع :
@@ -21,4 +21,21 @@ https://discord.gg/bcpjsSC
 
 }).catch(console.error)
 })
-A7MD.login('MzczMTc3MTc0MDM3MTAyNjAz.DrTL6g.QsYozZB_BAHCH2Bnjp4kalA6spQ');
+client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`***Konex shop***
+متجر لبيع :
+***نايترو***
+***حسابات ماين كرافت***
+***حسابات فورت نايت***
+***حسابات سبوتي فاي ***
+***تاقات مميزة***
+تخفيضات ليومين ***لحق نفسك*** 
+الرابط 
+https://discord.gg/bcpjsSC
+`)
+
+
+}).catch(console.error)
+})
+client.login('MzczMTc3MTc0MDM3MTAyNjAz.DrTL6g.QsYozZB_BAHCH2Bnjp4kalA6spQ');
