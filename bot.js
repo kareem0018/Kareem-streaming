01428c2dc4a36c384d92f38b6079c66cab530fcb
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-11-02 21:32:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,38 +4,12 @@ const client = new Discord.Client();
 console.log("BOT ONLINE");
 
 
-client.on("guildMemberAdd", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`***Konex shop***
-متجر لبيع :
-***نايترو***
-***حسابات ماين كرافت***
-***حسابات فورت نايت***
-***حسابات سبوتي فاي ***
-***تاقات مميزة***
-تخفيضات ليومين ***لحق نفسك*** 
-الرابط 
-https://discord.gg/bcpjsSC
-`)
+client.on('ready', () => {
+
+	
+
+        client.user.setGame('تم التهكير يا منوووووب',`http://www.youtube.com/gg`);	
+});
 
 
-}).catch(console.error)
-})
-client.on("guildMemberRemove", member => {
-  member.createDM().then(function (channel) {
-  return channel.send(`***Konex shop***
-متجر لبيع :
-***نايترو***
-***حسابات ماين كرافت***
-***حسابات فورت نايت***
-***حسابات سبوتي فاي ***
-***تاقات مميزة***
-تخفيضات ليومين ***لحق نفسك*** 
-الرابط 
-https://discord.gg/bcpjsSC
-`)
-
-
-}).catch(console.error)
-})
-client.login('NTA1NzgzNzI5MDk3Mjc3NDcy.DrY0WA.vcgo4-pu5hGxo-QCjj1iOAF27rw');
+client.login('NDk2MzAzNTYxNjIyMjkwNDQy.DpPevQ.Lq5US5CcgN6CJ3uY1DI-o6ntikM');
