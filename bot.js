749d36c7a6a31c049dbc29ca688a6b9fcf3449cc
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-11-03 21:05:57 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,35 +1,27 @@
-const Discord = require('discord.js');
+const tokenfile = require("./token.json")
+const Discord = require("discord.js");
 const client = new Discord.Client();
 
-console.log(`Logged in as ${client.user.username}#${client.user.discriminator}`);
+console.log("BOT ONLINE");
 
-
- var prefix = "+";
 
 client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('سباام يولد يلعن اومم الفله')
+          .then(m => {
+            count++;
+          })
 
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-  if(!args) return message.channel.send(`${prefix}say <words>`);
-
-if (command == "say") {
-
-message.channel.send(args.join("  "))
-    message.delete();
-  }
-
-
-
+        }
+      }
 });
 
 
 
-
-client.login('NDc3NTY3NzczNzQ5MjgwNzY4.DlAt1g.24xXCf783eQsJc9d4-WD15lG_W4');
+client.login("NTA4MzUwNDAwMzk5NjcxMjk2.Dr9-CA.0fLXCr8nIIOU0L2QYsoULTJdbsA");
 
 
 
